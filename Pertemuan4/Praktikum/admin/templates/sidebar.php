d2c66ea8e80687df4b843a7d5cb209cff8f20c80
unnnaaaa <hasnanurulap@gmail.com>
2023-03-23 18:01:10 +0700

    p4

---
+++ Pertemuan4/Praktikum/admin/templates/sidebar.php
@@ -0,0 +1,43 @@
+<?php
+
+$menu = [
+    'Home' => 'index.php',
+    'Produk' => 'produk.php',
+    'Daftar' => 'daftar.php',
+    'About' => 'about.php'
+
+];
+
+$icons = [
+    'Home' => 'house',
+    'Produk' => 'list',
+    'Daftar' => 'plus',
+    'About' => 'circle-info'
+
+];
+
+?>
+
+
+<div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                           
+                            <?php foreach($menu as $key => $value): ?>
+                            <a class="nav-link" href="<?= $value ?>">
+                                <div class="sb-nav-link-icon"><i class="fas fa-<?= $icons[$key] ?>"></i></div>
+                                <?= $key ?>
+                            </a>
+                           <?php endforeach ?>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
---
+++ Pertemuan4/Tugas/admin/daftar.php
@@ -0,0 +1,29 @@
+<?php
+include_once 'pages/header.php';
+include_once 'pages/navbar.php';
+include_once 'pages/sidebar.php';
+
+?>
+  <!-- /.container-fluid -->
+<main>
+<div class="content-wrapper bg-info"">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid>
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Silakan Isi Form Pendaftaran!</h1>
+          </div><!-- /.col -->
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+
+</main>
+
+  
+
+<?php
+include_once 'pages/footer.php';
